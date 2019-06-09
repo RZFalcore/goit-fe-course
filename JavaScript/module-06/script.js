db95e209906c6073bddd7770c51efb39214b3c61
RZFalcore <box.rz.box@gmail.com>
2019-06-09 18:31:15 +0300

    Finished module06

---
+++ JavaScript/module-06/script.js
@@ -0,0 +1,220 @@
+'use strick'
+
+class Notepad {
+  constructor(notes) {
+    this._notes = notes;
+  }
+
+  get notes () {
+    /*
+     * Принимает: ничего
+     * Возвращает: все заметки, значение свойства notes
+     */
+    return this._notes;
+  };
+
+  findNoteById (id) {
+    /*
+     * Ищет заметку в массиве notes
+     *
+     * Принимает: идентификатор заметки
+     * Возвращает: заметку с совпавшим полем id или undefined если ничего не найдено
+     */
+    return this.notes.find(note => note.id === id);
+  };
+
+  saveNote (note) {
+    /*
+     * Сохраняет заметку в массив notes
+     *
+     * Принимает: объект заметки
+     * Возвращает: сохраненную заметку
+     */
+    this._notes.push(note);
+    return note;
+  };
+
+  deleteNote (id) {
+    /*
+     * Удаляет заметку по идентификатору из массива notes
+     *
+     * Принимает: идентификатор заметки
+     * Возвращает: ничего
+     */
+    for (let x = 0; x < this._notes.length; x++) {
+      if (this._notes[x].id === id) {
+        this._notes.splice(x, 1);
+        break;
+      }
+    }
+  };
+  updateNoteContent (id, updatedContent) {
+    /*
+     * Обновляет контент заметки
+     * updatedContent - объект с полями вида {имя: значение, имя: значение}
+     * Свойств в объекте updatedContent может быть произвольное количество
+     *
+     * Принимает: идентификатор заметки и объект, полями которого надо обновить заметку
+     * Возвращает: обновленную заметку
+     */
+    for (let x in this._notes) {
+      if (x.id === id) {
+        Object.assign(x, updatedContent);
+        break;
+      }
+    }
+  };
+
+  updateNotePriority (id, priority) {
+    /*
+     * Обновляет приоритет заметки
+     *
+     * Принимает: идентификатор заметки и ее новый приоритет
+     * Возвращает: обновленную заметку
+     */
+    for (let key of this._notes) {
+      key.id === id ? key.priority = priority : null;
+    }
+  };
+
+  filterNotesByQuery (query) {
+    /*
+     * Фильтрует массив заметок по подстроке query.
+     * Если значение query есть в заголовке или теле заметки - она подходит
+     *
+     * Принимает: подстроку для поиска в title и body заметки
+     * Возвращает: новый массив заметок, контент которых содержит подстроку
+     */
+    const arr = [];
+
+    for (let key of this._notes) {
+      key.title.toLowerCase().includes(query) || key.title.toLowerCase().includes(query) ? arr.push(key) : null
+    }
+    return arr
+  };
+
+  filterNotesByPriority (priority) {
+    /*
+     * Фильтрует массив заметок по значению приоритета
+     * Если значение priority совпадает с приоритетом заметки - она подходит
+     *
+     * Принимает: приоритет для поиска в свойстве priority заметки
+     * Возвращает: новый массив заметок с подходящим приоритетом
+     */
+    const arr = [];
+    for (let x of this._notes) {
+      if (x.priority === priority) {
+        arr.push(x);
+      }
+    }
+    return arr
+  }
+
+ static Priority = {
+  LOW: 0,
+  NORMAL: 1,
+  HIGH: 2,
+};
+
+
+}
+const initialNotes = [
+  {
+    id: 'id-1',
+    title: 'JavaScript essentials',
+    body:
+      'Get comfortable with all basic JavaScript concepts: variables, loops, arrays, branching, objects, functions, scopes, prototypes etc',
+    priority: Notepad.Priority.HIGH,
+  },
+  {
+    id: 'id-2',
+    title: 'Refresh HTML and CSS',
+    body:
+      'Need to refresh HTML and CSS concepts, after learning some JavaScript. Maybe get to know CSS Grid and PostCSS, they seem to be trending.',
+    priority: Notepad.Priority.NORMAL,
+  },
+];
+
+const notepad = new Notepad(initialNotes);
+
+/*
+  Смотрю что у меня в заметках после инициализации
+*/
+console.log('Все текущие заметки: ', notepad.notes);
+
+/*
+ * Добавляю еще 2 заметки и смотрю что получилось
+ */
+notepad.saveNote({
+  id: 'id-3',
+  title: 'Get comfy with Frontend frameworks',
+  body:
+    'First must get some general knowledge about frameworks, then maybe try each one for a week or so. Need to choose between React, Vue and Angular, by reading articles and watching videos.',
+  priority: Notepad.Priority.NORMAL,
+});
+
+notepad.saveNote({
+  id: 'id-4',
+  title: 'Winter clothes',
+  body:
+    "Winter is coming! Need some really warm clothes: shoes, sweater, hat, jacket, scarf etc. Maybe should get a set of sportwear as well so I'll be able to do some excercises in the park.",
+  priority: Notepad.Priority.LOW,
+});
+
+console.log('Все текущие заметки: ', notepad.notes);
+
+/*
+ * Зима уже близко, пора поднять приоритет на покупку одежды
+ */
+notepad.updateNotePriority('id-4', Notepad.Priority.NORMAL);
+
+console.log('Заметки после обновления приоритета для id-4: ', notepad.notes);
+
+/*
+ * Решил что фреймворки отложу немного, понижаю приоритет
+ */
+notepad.updateNotePriority('id-3', Notepad.Priority.LOW);
+
+console.log('Заметки после обновления приоритета для id-3: ', notepad.notes);
+
+/*
+ * Решил отфильтровать заметки по слову html
+ */
+console.log(
+  'Отфильтровали заметки по ключевому слову "html": ',
+  notepad.filterNotesByQuery('html'),
+);
+
+/*
+ * Решил отфильтровать заметки по слову javascript
+ */
+console.log(
+  'Отфильтровали заметки по ключевому слову "javascript": ',
+  notepad.filterNotesByQuery('javascript'),
+);
+
+/*
+ * Хочу посмотреть только заметки с нормальным приоритетом
+ */
+console.log(
+  'Отфильтровали заметки по нормальному приоритету: ',
+  notepad.filterNotesByPriority(Notepad.Priority.NORMAL),
+);
+
+/*
+ * Обновим контент заметки с id-3
+ */
+notepad.updateNoteContent('id-3', {
+  title: 'Get comfy with React.js or Vue.js',
+});
+
+console.log(
+  'Заметки после обновления контента заметки с id-3: ',
+  notepad.notes,
+);
+
+/*
+ * Повторил HTML и CSS, удаляю запись c id-2
+ */
+notepad.deleteNote('id-2');
+console.log('Заметки после удаления с id -2: ', notepad.notes);
